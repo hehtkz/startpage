91be7c70ef2ca05bb92eecc5a6d89ef87cdbe372
Wellington Viana <viana.swe@gmail.com>
2017-12-07 21:25:25 -0200

    formatação do código

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-var sitesUrl = ["https://news.ycombinator.com/",
-  "http://pt.stackoverflow.com/", "https://trello.com/", "https://nixers.net",
+var sitesUrl = [
+  "https://news.ycombinator.com/",
+  "http://pt.stackoverflow.com/",
+  "https://trello.com/",
+  "https://nixers.net",
   "https://bbs.archlinux.org/"
 ];
 
 var sitesNames = ["hn", "so", "trello", "nixers", "bbs"];
 
-var sitesSearch = ["http://pt.stackoverflow.com/search?q=",
+var sitesSearch = [
+  "http://pt.stackoverflow.com/search?q=",
   "https://www.youtube.com/results?search_query=",
   "https://github.com/search?&q=",
   "https://thepiratebay.se/search/",
@@ -16,35 +20,51 @@ var sitesSearch = ["http://pt.stackoverflow.com/search?q=",
   "http://gen.lib.rus.ec/search.php?req="
 ];
 
-var sitesNIDSearch = ["stack-ov", "youtube", "github", "pirate-bay", "dicio",
-  "reddit", "caniuse", "archwiki", "libgen"
+var sitesNIDSearch = [
+  "stack-ov",
+  "youtube",
+  "github",
+  "pirate-bay",
+  "dicio",
+  "reddit",
+  "caniuse",
+  "archwiki",
+  "libgen"
 ];
 
-var sitesNSearch = ["Stack Overflow", "Youtube", "GitHub", "The Pirate Bay",
-  "Dicio", "Reddit", "Can I Use", "ArchWiki", "Library Genesis"
+var sitesNSearch = [
+  "Stack Overflow",
+  "Youtube",
+  "GitHub",
+  "The Pirate Bay",
+  "Dicio",
+  "Reddit",
+  "Can I Use",
+  "ArchWiki",
+  "Library Genesis"
 ];
 
-var conLink = document.getElementById('container-links');
+var conLink = document.getElementById("container-links");
 for (i = 0; i < sitesNames.length; i++) {
-  var link = document.createElement('a');
-  link.setAttribute('href', sitesUrl[i]);
-  link.setAttribute('class', 'link');
+  var link = document.createElement("a");
+  link.setAttribute("href", sitesUrl[i]);
+  link.setAttribute("class", "link");
   link.text = sitesNames[i];
   conLink.appendChild(link);
 }
-var conSearch = document.getElementById('container-search');
+var conSearch = document.getElementById("container-search");
 for (var i = 0; i < sitesNSearch.length; i++) {
-  var searchBox = document.createElement('input');
-  searchBox.setAttribute('placeholder', sitesNSearch[i]);
+  var searchBox = document.createElement("input");
+  searchBox.setAttribute("placeholder", sitesNSearch[i]);
   searchBox.id = sitesNIDSearch[i];
   conSearch.appendChild(searchBox);
 }
 
-document.body.setAttribute('class', 'load');
+document.body.setAttribute("class", "load");
 
 var input = document.getElementsByTagName("input");
 for (var i = 0; i < input.length; i++) {
-  input[i].addEventListener('keydown', function(event) {
+  input[i].addEventListener("keydown", function(event) {
     var tecla = event.keyCode;
     if (tecla == 13) {
       var pos = sitesNIDSearch.indexOf(this.id);
